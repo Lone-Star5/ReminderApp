d033d0a40ba290c9f9045dddc7055546ef4859f3
Lone-Star5 <amankap05@gmail.com>
2021-08-04 00:36:47 +0530

    added calendar using datepicker

--- app/src/main/java/com/example/reminderapp/CreateReminder.kt
+++ app/src/main/java/com/example/reminderapp/CreateReminder.kt
@@ -2,21 +2,40 @@ package com.example.reminderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
+import java.util.*
 
 class CreateReminder : AppCompatActivity() {
 
     private lateinit var createReminderBtn : Button
+    private lateinit var title : EditText
+    private lateinit var description: EditText
+    private lateinit var date: DatePicker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_reminder)
 
         createReminderBtn = findViewById(R.id.create_reminder_btn)
+        title = findViewById(R.id.addTitle)
+        description = findViewById(R.id.addDescription)
+        date = findViewById(R.id.addDate)
+
+        var dateText = ""
+
+        val today = Calendar.getInstance()
+        date.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH))
+        { view, year, month, day ->
+            val month = month + 1
+            dateText = "$day/$month/$year"
+        }
 
         createReminderBtn.setOnClickListener {
-            Toast.makeText(this, "Feature Under Development", Toast.LENGTH_SHORT).show()
+            val titleText = title.text
+            val descriptionText = description.text
+
+            val msg = "$titleText $descriptionText $dateText"
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
         }
     }
 }
